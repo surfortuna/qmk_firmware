9cd24c91d08bf90707e273297fa9c1370ab424b3
Ian Fortuna <surfortuna@gmail.com>
2020-05-22 18:54:09 -0700

    Updating my kbd67 keymap.

--- keyboards/kbdfans/kbd67/rev2/keymaps/surfortuna/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/surfortuna/keymap.c
@@ -39,42 +39,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______, _______, \
   _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______, _______, \
   _______,XXXXXXX,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, \
-  _______, _______, _______,          XXXXXXX,           _______,        XXXXXXX,           _______, _______,XXXXXXX, _______, _______, _______),    
+  _______, _______, _______,          XXXXXXX,           _______,        XXXXXXX,           _______, _______, XXXXXXX, _______, _______, _______),    
 
  *
  */ 
 
 [_BL] = LAYOUT_all(
-  KC_ESC ,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_MINS, KC_EQL, KC_BSLS,KC_GRAVE,  KC_DEL, \
-  KC_TAB ,       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,  KC_RBRC,     KC_BSPC, KC_PGUP, \
-  MO(_FL),          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,           KC_ENT, KC_PGDN, \
-  KC_LSFT,  XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,         KC_RSFT, KC_UP, MO(_FL), \
-  KC_LCTL,  KC_LGUI,   KC_LALT,          XXXXXXX,           KC_SPC,        XXXXXXX,           KC_RALT, KC_RGUI,XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT),  
+  KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRAVE, KC_DEL, \
+  KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC, KC_PGUP, \
+  MO(_FL), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN, \
+  KC_LSFT, XXXXXXX, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, MO(_FL), \
+  KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, KC_SPC, XXXXXXX, KC_RALT, KC_RGUI, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT),  
 
   /* Keymap Fn Layer
    * ,----------------------------------------------------------------.
-   * |   |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|   |Rst|Ins |
+   * |~ `|F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Prt|   |Ins |
    * |----------------------------------------------------------------|
-   * |Caps |Mut|VDn|VUp|   |   |   |   |PSc|SLk|Pau|Up |    |Calc|Home|
+   * |Caps |Mut|VDn|VUp|   |   |   |   |   |   |   |PUp|    |Calc|Home|
    * |----------------------------------------------------------------|
-   * |      |Prv|Ply|Nxt|   |   |   |   |Hom|PUp|Lef|Rig|        |End |
+   * |      |Prv|Ply|Nxt|   |   |Lft|Dwn|Up |Rgt|Hom|End|        |End |
    * |----------------------------------------------------------------|
-   * |        |   |   |   |   |   |   |   |End|PDn|Dow|      |PUp|    |
+   * |        |   |   |   |   |   |   |   |   |   |PDn|      |PUp|    |
    * |----------------------------------------------------------------|
-   * |    |    |    |                       |   |   |    |Hom|PDn|End |
+   * |    |    |    |                       |   |Rst|    |Hom|PDn|End |
    * `----------------------------------------------------------------'
    */
 
 [_FL] = LAYOUT_all(
-  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12, _______,RESET,  KC_INS, \
-  KC_CAPS,     KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS,   KC_UP, _______,      KC_CALC, KC_HOME, \
-  _______,       KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,             _______,  KC_END, \
-  _______,XXXXXXX,    _______, _______, _______, _______, _______, _______, _______,  KC_END, KC_PGDN, KC_DOWN,        _______, KC_PGUP, _______, \
-  _______, _______, _______,          XXXXXXX,           _______,        XXXXXXX,           _______, _______,XXXXXXX, KC_HOME,  KC_PGDN, KC_END),  
+  KC_GRAVE, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, _______, KC_INS, \
+  KC_CAPS, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, _______, KC_CALC, KC_HOME, \
+  _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_HOME, KC_END, _______, KC_END, \
+  _______, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, _______, KC_PGUP, _______, \
+  _______, _______, _______, XXXXXXX, _______, XXXXXXX, _______, RESET, XXXXXXX, KC_HOME, KC_PGDN, KC_END),  
 
   /* Keymap Numpad Layer
    * ,----------------------------------------------------------------.
-   * |_BL|   |   |   |   |   |/  |7  |8  |9  |-  |   |   |   |Del|Rset|
+   * |_BL|   |   |   |   |   |/  |7  |8  |9  |-  |   |   |   |   |Del |
    * |----------------------------------------------------------------|
    * |Tab  |MLC|MUp|MRC|   |   |*  |4  |5  |6  |+  |   |    |BS  |Calc|
    * |----------------------------------------------------------------|
@@ -87,11 +87,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
 [_NL] = LAYOUT_all(
-  TG(_NL),   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_PSLS,   KC_P7,   KC_P8,   KC_P9, KC_PMNS,   KC_NO,   KC_NO,   KC_NO,   RESET,  KC_DEL, \
-  KC_TAB ,     KC_BTN1, KC_MS_U, KC_BTN2,   KC_NO,   KC_NO, KC_PAST,   KC_P4,   KC_P5,   KC_P6, KC_PPLS,   KC_NO,   KC_NO,      KC_BSPC, KC_CALC, \
-    KC_NO,       KC_MS_L, KC_MS_D, KC_MS_R,   KC_NO,   KC_NO,   KC_NO,   KC_P1,   KC_P2,   KC_P3, KC_PENT,   KC_NO,             KC_ENT ,   KC_NO, \
-    KC_NO,XXXXXXX,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P0, KC_PDOT, KC_PEQL,          KC_NO, KC_UP  ,   KC_NO, \
-  RGB_TOG, BL_TOGG,  BL_STEP,          XXXXXXX,            KC_SPC,        XXXXXXX,             KC_NO,   KC_NO,XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT), 
+  TG(_NL), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSLS, KC_P7, KC_P8, KC_P9, KC_PMNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_DEL, \
+  KC_TAB, KC_BTN1, KC_MS_U, KC_BTN2, KC_NO, KC_NO, KC_PAST, KC_P4, KC_P5, KC_P6, KC_PPLS, KC_NO, KC_NO, KC_BSPC, KC_CALC, \
+  KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_PENT, KC_NO, KC_ENT, KC_NO, \
+  KC_NO, XXXXXXX, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P0, KC_PDOT, KC_PEQL, KC_NO, KC_UP, KC_NO, \
+  RGB_TOG, BL_TOGG, BL_STEP, XXXXXXX, KC_SPC, XXXXXXX, KC_NO, KC_NO, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT), 
 
 };
 
